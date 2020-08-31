ca19dc62974fe31036616ed79ccc58b3bdbd7a4c
admin <kryschtopenok@ukr.net>
2020-08-31 20:26:19 +0300

    First commit

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -4,8 +4,8 @@ import com.company.task2.Humen;
 
 public class Main{
     public static void main(String[] args) {
-       Humen as = new Humen("DIMA",13,145,35.8) ;
-               as.say();
+       Humen humen = new Humen("DIMA",13,145,35.8) ;
+               humen.say();
     }
 }
 

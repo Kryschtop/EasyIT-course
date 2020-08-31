310b4bbdb0e603b8a9949c107f495bcf82a63b20
admin <kryschtopenok@ukr.net>
2020-08-31 20:04:07 +0300

    First commit

---
+++ src/com/company/task2/Humen.java
@@ -0,0 +1,24 @@
+package com.company.task2;
+
+public class Humen {
+    public String name;
+    public int age;
+    public double height;
+    public double weight;
+
+    public Humen(String name,int age,double height,double weight){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+
+    }
+
+public  void say(){
+        System.out.println(
+                "Name: " + name + "\n" +
+                        "Height: " + height +
+                        "Age: " + age +
+                        "Weight: " + weight +);
+}
+}
---
+++ src/com/company/task2/Main.java
@@ -0,0 +1,15 @@
+package com.company.task2;
+
+import com.company.task2.Humen;
+
+public class Main{
+    public static void main(String[] args) {
+       Humen as = new Humen("DIMA",13,145,35.8) ;
+               as.say();
+    }
+}
+
+
+
+
+

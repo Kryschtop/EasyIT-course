e0312ee8f74ebf3a92e7ddb97267e2251557ae25
admin <kryschtopenok@ukr.net>
2020-09-02 12:04:43 +0300

    HOME COMMINT

--- src/com/company/task2/Boy.java
+++ src/com/company/task2/Boy.java
@@ -1,4 +1,16 @@
 package com.company.task2;
 
-public class Boy {
+    public class Boy extends Humen{
+
+    public Boy(String name,int age,double height,double weight)
+    {
+        super(name, age, height, weight);
+
+    }
+
+        public void say(){
+        super.say();
+    }
+
 }
+
--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -2,12 +2,20 @@ package com.company.task2;
 
 import com.company.task2.Humen;
 
-public class Main{
+public class Main {
+
     public static void main(String[] args) {
-       Humen girl = new Girl("DIMA",13,145,35.8) ;
-               girl.say();
-    }
-}
+
+                Humen humen = new Humen("DIMA",13,145,35.8) ;
+                humen.say();
+                Humen girl = new Girl("Ira",34,165,55.8) ;
+                girl.say();
+                Humen boy = new Boy("OLEG",25,178,76);
+                boy.say();
+            }
+        }
+
+
 
 
 

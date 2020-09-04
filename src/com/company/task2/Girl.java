46f9d11c299299b40c24d1460f04fc86fe2a5289
kryschtop <kryschtopenok@ukr.net>
2020-09-04 20:17:36 +0300

    First commit

--- src/com/company/task2/Girl.java
+++ src/com/company/task2/Girl.java
@@ -1,14 +1,16 @@
 package com.company.task2;
 
 public class Girl extends Humen{
-
-    public Girl(String name,int age,double height,double weight)
+    private Hobby hobby;
+    public Girl(String name,int age,double height,double weight,Hobby hobby)
     {
         super(name, age, height, weight);
-
+        this.hobby= hobby;
     }
     public void say(){
         super.say();
     }
-
+public  Hobby getHobby(){
+    return hobby;
+}
 }
